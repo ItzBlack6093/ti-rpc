cc98396b0c85a473c47f1f4df8e64d21c6a3e4e6
Vy Xuân Phúc <d4.blackcat@gmail.com>
2025-08-16 07:56:18 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,102 @@
+from PyMemoryEditor import OpenProcess
+from pypresence import Presence
+import time
+import math
+
+client_id = 1307579689522434078
+RPC = Presence(client_id)
+RPC.connect()
+
+isGameDetected = 0
+
+modeAddress = 0x4AE32A
+modAddress = 0x4AE32C
+levelAddress = 0x4AE300
+sectionAddress = 0x4ACD8A
+sakuraStageAddress = 0x4AE331
+
+def getModName(value):
+    match value:
+        case 1:
+            return '20G'
+        case 2:
+            return 'Big'
+        case 8:
+            return 'TLS'
+        case _:
+            return ''
+def getModeName(value):
+    match value:
+        case 1:
+            return 'Easy'
+        case 2:
+            return 'Master'
+        case 32:
+            return 'Shirase'
+        case 64:
+            return 'Sakura'
+        case 514:
+            return 'Promotional Exam'
+        case 4098:
+            return 'Demotional Exam'
+        case 258:
+            return 'Attract Screen'
+        case _:
+            return 'Nothing'    
+        
+def getModeKey(value):
+    match value:
+        case 1:
+            return 'easy'
+        case 2:
+            return 'master'
+        case 32:
+            return 'shirase'
+        case 64:
+            return 'sakura'
+        case 514:
+            return 'qualify'
+        case 4098:
+            return 'demotional'
+        case _:
+            return 'logo'
+
+def getSakuraStage(value):
+    stage = value % 256 + 1
+    clearedStage = math.floor(value / 256)
+    if stage > 20:
+        return "EX" + str(stage - 20)
+    return stage
+
+while True:
+    try:
+        with OpenProcess(process_name = "game.exe") as process:
+            if isGameDetected != 1:
+                print('Detected game.exe')
+                isGameDetected = 1
+
+            modeValue = process.read_process_memory(modeAddress, int, 2)
+            modValue =  process.read_process_memory(modAddress, int, 2)
+            level = process.read_process_memory(levelAddress, int, 2)
+            section = process.read_process_memory(sectionAddress, int, 2)
+
+            state = "Level " + str(level) + " / " + str((section + 1) * 100)
+
+            if modeValue == 64:
+                sakuraLevelValue = process.read_process_memory(sakuraStageAddress, int, 2)
+                state = state + " (Stage " + str(getSakuraStage(sakuraLevelValue)) + ")"
+            if modeValue == 258:
+                state = 'Chilling'
+
+            RPC.update(
+                state = state,
+                details = getModName(modValue) + " " + getModeName(modeValue),
+                small_image = getModeKey(modeValue),
+                large_image = 'logo',
+            )
+    except:
+        if isGameDetected != -1:
+            print('Cannot detect game.exe. Please open the game')
+            isGameDetected = -1
+    time.sleep(0.5)
+
